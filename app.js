36113a897c6ade5c64074e065edacae7f8f76b74
Joel Ndiba Mwaura <mwa21001@byui.edu>
2024-04-03 15:34:01 +0300

    commit

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
-
 var users = require('./routes/users');
-
+const mongoose = require('mongoose');
 var app = express();
+
+
+
+const uri = 'mongodb://localhost:27017/mydatabase';
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
 
 mongodb.initDb((err) => {
     if (err) {
